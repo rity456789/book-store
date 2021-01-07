fcb77ce7e0a3b1bda11eb4808c3c11bbd7596f29
phbinh <rity456789@gmail.com>
2021-01-07 11:28:01 +0700

    route in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import history from "./ultis/history";
+import { Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
+import { SRoute } from "./containers/CustomRoutes";
+
+import Home from "./containers/Home/Home";
+import Login from "./containers/Login/Login";
+import Register from "./containers/Register/Register";
+import NotFound from "./containers/NotFound";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <>
+      <Router>
+        <Switch history={history}>
+          <SRoute path="/login" exact component={Login}></SRoute>
+          <SRoute path="/register" exact component={Register}></SRoute>
+          <SRoute path="/not-found" component={NotFound}></SRoute>
+          <SRoute path="/" exact component={Home}></SRoute>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
