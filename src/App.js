ee15814885697d4666e5046b9c6c32315a459ab4
phbinh <rity456789@gmail.com>
2021-01-09 10:55:09 +0700

    add login UI

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { SRoute } from "./containers/CustomRoutes";
 
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
-import Register from "./containers/Register/Register";
 import NotFound from "./containers/NotFound";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Router>
         <Switch history={history}>
           <SRoute path="/login" exact component={Login}></SRoute>
-          <SRoute path="/register" exact component={Register}></SRoute>
           <SRoute path="/not-found" component={NotFound}></SRoute>
           <SRoute path="/" exact component={Home}></SRoute>
           <Redirect to="/not-found"></Redirect>
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,7 +1,204 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-10 col-xl-8">
+          <div className="accordion" id="accordion1">
+            <div className="card card-panel bg-white shadow-sm mb-2">
+              <div
+                className="card-header py-4 bg-white"
+                role="button"
+                id="headingOne"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                <h2 className="h4 mb-0">Register now</h2>
+              </div>
+              <div
+                id="collapseOne"
+                className="collapse show"
+                aria-labelledby="headingOne"
+                data-parent="#accordion1"
+              >
+                <div className="card-body p-0">
+                  <hr className="m-0" />
+                  <div className="p-4 p-md-6">
+                    <form>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="First name: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Last name: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Company name:"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Address: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Email: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Phone: *"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="custom-control custom-checkbox custom-checkbox-primary py-2">
+                            <input
+                              type="checkbox"
+                              className="custom-control-input"
+                              id="customExampleCheck1"
+                            />
+                            <label
+                              className="custom-control-label"
+                              htmlFor="customExampleCheck1"
+                            >
+                              I have read and accepted the{" "}
+                              <a href="#">terms and conditions</a>
+                            </label>
+                          </div>
+                          <div className="custom-control custom-checkbox custom-checkbox-primary py-2">
+                            <input
+                              type="checkbox"
+                              className="custom-control-input"
+                              id="customExampleCheck2"
+                            />
+                            <label
+                              className="custom-control-label"
+                              htmlFor="customExampleCheck2"
+                            >
+                              Subscribe to exciting newsletters and great tips
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-12 text-right py-4">
+                          <a
+                            href="checkout-delivery.html"
+                            className="btn btn-primary"
+                          >
+                            Create an account
+                          </a>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Checkout login */}
+            <div className="card card-panel bg-white shadow-sm mb-2">
+              <div
+                className="card-header py-4 collapsed bg-white"
+                role="button"
+                id="headingTwo"
+                data-toggle="collapse"
+                data-target="#collapseTwo1"
+                aria-expanded="false"
+                aria-controls="collapseTwo1"
+              >
+                <h2 className="h4 mb-0"> Already member?</h2>
+              </div>
+              <div
+                id="collapseTwo1"
+                className="collapse"
+                aria-labelledby="headingTwo"
+                data-parent="#accordion1"
+              >
+                <div className="card-body p-0">
+                  <hr className="m-0" />
+                  <div className="p-4 p-md-6">
+                    <form>
+                      <div className="row justify-content-center py-4">
+                        <div className="col-md-8">
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-control form-control-simple"
+                              placeholder="Email"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <input
+                              type="password"
+                              className="form-control form-control-simple"
+                              placeholder="Password"
+                            />
+                          </div>
+                          <div className="custom-control custom-checkbox custom-checkbox-primary py-2">
+                            <input
+                              type="checkbox"
+                              className="custom-control-input"
+                              id="rememberCheck"
+                            />
+                            <label
+                              className="custom-control-label"
+                              htmlFor="rememberCheck"
+                            >
+                              Remember me
+                            </label>
+                          </div>
+                          <div className=" text-right py-4">
+                            <a
+                              href="checkout-delivery.html"
+                              className="btn btn-dark"
+                            >
+                              Sign in
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
